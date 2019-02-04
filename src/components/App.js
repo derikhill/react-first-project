f99528ff68d30d12684e9fb0f44dd90d2bea854e
derikhill <derikhill05@gmail.com>
2019-02-03 21:52:25 -0600

    Adding state, working through building first react project.

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,29 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+    addFish = fish => {
+        // taking a copy of existing state
+        const fishes = { ...this.state.fishes };
+        // adding new fish to existing fishes object
+        fishes[`fish${Date.now()}`] = fish;
+        // set new fishes object to state
+        this.setState({
+            fishes
+           // above is same as saying  fishes: fishes
+        });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish={this.addFish}/>
             </div>
         )
     }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { format } from 'url';
+import { getFunName } from '../helpers';
+//import { format } from 'url';
 
 class StorePicker extends React.Component {
+    theInput = React.createRef();
+
+    goToStore = event => {
+        event.preventDefault();
+        const storeName = this.theInput.current.value;
+        this.props.history.push(`/store/${storeName}`)
+    };
+    
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input 
+                    type="text" 
+                    ref={this.theInput}
+                    required 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()}
+                />
                 <button type="submit">Visit Store -></button>
             </form>
-        )
+        );
     }
 }
 
